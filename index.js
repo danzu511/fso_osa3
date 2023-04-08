214102bb5ff1c07b32b8ca1005e2f5d6754e9e02
danzu511 <daneriklundstrom@gmail.com>
2023-04-08 14:51:10 +0300

    scriptcommit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+
 morgan.token('body', function getBody(req){
     //if statement to clean {} from morgan output 
     if(JSON.stringify(req.body) !== '{}'){
